3816ea25654e4c57c697a11d676a7443813c1093
Ahmad Syauqi <syauqeesy@gmail.com>
2023-12-31 14:16:08 +0700

    chore: setup entity & repository

---
+++ src/entity/User.ts
@@ -0,0 +1,59 @@
+import { BaseEntity, Column, PrimaryGeneratedColumn } from "typeorm";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string = uuid();
+
+  @Column({
+    type: "varchar",
+    length: 32,
+    nullable: false,
+  })
+  username!: string;
+
+  @Column({
+    type: "text",
+    nullable: false,
+  })
+  password!: string;
+
+  @Column({
+    type: "bigint",
+    nullable: false,
+  })
+  created_at: number = Date.now();
+
+  @Column({
+    type: "bigint",
+    nullable: true,
+  })
+  updated_at: number | null = null;
+
+  @Column({
+    type: "bigint",
+    nullable: true,
+  })
+  deleted_at: number | null = null;
+
+  public setUsername(username: string): void {
+    this.username = username;
+  }
+
+  public setPassword(password: string): void {
+    const salt = bcrypt.genSaltSync(10);
+
+    this.password = bcrypt.hashSync(password, salt);
+  }
+
+  public comparePassword(password: string): boolean {
+    return bcrypt.compareSync(password, this.password);
+  }
+
+  public getCreatedAt(): string {
+    return new Date(this.created_at).toISOString();
+  }
+}
+
+export default User;
---
+++ src/entity/main.ts
@@ -0,0 +1,17 @@
+import { DataSource, Repository } from "typeorm";
+
+import User from "./User";
+
+type repository = {
+  user: Repository<User>;
+};
+
+const newRepository = (dataSource: DataSource): repository => {
+  const r: repository = {
+    user: dataSource.manager.getRepository(User),
+  };
+
+  return r;
+};
+
+export { newRepository, User as UserEntity };
